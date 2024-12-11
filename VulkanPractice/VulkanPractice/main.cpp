e9826d2875cd3b2f83e4d4b10ab4f61b76177a96
Sourav Chatterjee <sromic@gmail.com>
2024-12-11 12:20:42 +0200

    Minor commit. Adding buffer flush to cout.

--- VulkanPractice/VulkanPractice/main.cpp
+++ VulkanPractice/VulkanPractice/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[])
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
     
-    std::cout << extensionCount << " extensions supported\n";
+    std::cout << extensionCount << " extensions supported\n" << std::endl;
     
     glm::mat4 matrix;
     glm::vec4 vec;
